6a2ae2587733f53df1a734bab7731d52196f380b
Adrian Kennard <a@k.gg>
2020-12-05 16:16:30 +0000

    Auto power control via sonoff mqtt now

--- main/ASR33.c
+++ main/ASR33.c
@@ -6,26 +6,80 @@ static const char TAG[] = "ASR33";
 #include <esp_spi_flash.h>
 #include <driver/uart.h>
 
-static char *otaurl;
+
+#define settings  \
+  u8(uart,1);	\
+  u8(tx,1);	\
+  u8(rx,3);	\
+  t(sonoff);	\
+  u32(idle,60)	\
+
+#define u32(n,d) uint32_t n;
+#define u16(n,d) uint16_t n;
+#define u8(n,d) uint8_t n;
+#define u1(n) uint8_t n;
+#define t(n) const char*n=NULL;
+settings
+#undef t
+#undef u32
+#undef u16
+#undef u8
+#undef u1
+    uint32_t last;
+
+void turn_off(void)
+{
+   last = 0;
+   if (sonoff)
+      revk_raw(NULL, sonoff, 1, "0", 0);
+}
+
+void turn_on(void)
+{
+   if (last)
+      return;
+   last = time(0);
+   if (sonoff)
+      revk_raw(NULL, sonoff, 1, "1", 0);
+}
 
 const char *app_command(const char *tag, unsigned int len, const unsigned char *value)
 {
-   if (!strcmp(tag, "wifi") && *otaurl)
-   {
-      revk_ota(otaurl);
-      revk_setting("otaurl", 0, NULL);  // Clear the URL
-   }
    if (!strcmp(tag, "connect"))
+   {
       revk_info(TAG, "Running ASR33 control");
+      turn_off();
+   }
+   if (!strcmp(tag, "upgrade"))
+      turn_off();
+   if (!strcmp(tag, "on"))
+      turn_on();
+   if (!strcmp(tag, "off"))
+      turn_off();
    if (!strcmp(tag, "tx"))
-	   uart_write_bytes(0,value,len);
+   {
+      turn_on();
+      uart_write_bytes(uart, value, len);
+   }
    return "";
 }
 
 void app_main()
 {
    revk_init(&app_command);
-   revk_register("otaurl", 0, 0, &otaurl, NULL, 0);
+#define u32(n,d) revk_register(#n,0,sizeof(n),&n,#d,0);
+#define u16(n,d) revk_register(#n,0,sizeof(n),&n,#d,0);
+#define u8(n,d) revk_register(#n,0,sizeof(n),&n,#d,0);
+#define u1(n) revk_register(#n,0,sizeof(n),&n,NULL,SETTING_BOOLEAN);
+#define t(n) revk_register(#n,0,0,&n,NULL,0);
+   settings
+#undef t
+#undef u32
+#undef u16
+#undef u8
+#undef u1
+       turn_off();
+   ESP_ERROR_CHECK(uart_driver_install(uart, 1024, 1024, 0, NULL, 0));
    uart_config_t uart_config = {
       .baud_rate = 110,
       .data_bits = UART_DATA_8_BITS,
@@ -33,11 +87,16 @@ void app_main()
       .stop_bits = UART_STOP_BITS_2,
       .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
    };
-   uart_param_config(0, &uart_config);
-   uart_set_line_inverse(0, UART_SIGNAL_RXD_INV);
-   esp_log_level_set("*",ESP_LOG_ERROR);
+// Configure UART parameters
+   ESP_ERROR_CHECK(uart_param_config(uart, &uart_config));
+   ESP_ERROR_CHECK(uart_set_pin(uart, tx, rx, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+
    while (1)
    {
-	   sleep(1);
+      usleep(90000);
+      time_t now = time(0);
+      if (last && now > last + idle)
+         turn_off();
+      // TODO ready byte
    }
 }
