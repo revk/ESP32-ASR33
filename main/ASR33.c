912b1fe1d948759db5cde856f159db520ac93a39
Adrian Kennard <a@k.gg>
2020-12-05 08:53:31 +0000

    INitial testing

--- main/ASR33.c
+++ main/ASR33.c
@@ -1,30 +1,43 @@
-// OTA app, can be used to do an OTA from a small footprint. Also does partition update.
-// Warning, moving NVS will typically mess reset all of the settings
+// ASR33 direct control over MQTT
 // Copyright © 2019 Adrian Kennard, Andrews & Arnold Ltd. See LICENCE file for details. GPL 3.0
-static const char TAG[] = "OTA";
+static const char TAG[] = "ASR33";
 
 #include "revk.h"
 #include <esp_spi_flash.h>
+#include <driver/uart.h>
 
 static char *otaurl;
 
-const char *
-app_command (const char *tag, unsigned int len, const unsigned char *value)
+const char *app_command(const char *tag, unsigned int len, const unsigned char *value)
 {
-   if (!strcmp (tag, "wifi") && *otaurl)
+   if (!strcmp(tag, "wifi") && *otaurl)
    {
-      revk_ota (otaurl);
-      revk_setting ("otaurl", 0, NULL); // Clear the URL
+      revk_ota(otaurl);
+      revk_setting("otaurl", 0, NULL);  // Clear the URL
    }
-   if (!strcmp (tag, "connect"))
-      revk_info (TAG, "Running generic OTA system");
+   if (!strcmp(tag, "connect"))
+      revk_info(TAG, "Running ASR33 control");
+   if (!strcmp(tag, "tx"))
+	   uart_write_bytes(0,value,len);
    return "";
 }
 
-void
-app_main ()
+void app_main()
 {
-   revk_init (&app_command);
-   revk_register ("otaurl", 0, 0, &otaurl, NULL, 0);
-   // Note, this used to do partition checks, but now done in revk library
+   revk_init(&app_command);
+   revk_register("otaurl", 0, 0, &otaurl, NULL, 0);
+   uart_config_t uart_config = {
+      .baud_rate = 110,
+      .data_bits = UART_DATA_8_BITS,
+      .parity = UART_PARITY_DISABLE,
+      .stop_bits = UART_STOP_BITS_2,
+      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+   };
+   uart_param_config(0, &uart_config);
+   uart_set_line_inverse(0, UART_SIGNAL_RXD_INV);
+   esp_log_level_set("*",ESP_LOG_ERROR);
+   while (1)
+   {
+	   sleep(1);
+   }
 }
