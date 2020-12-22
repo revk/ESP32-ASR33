c1a2ae749902d2e348ccde0ff0064341e3052cd6
Adrian Kennard <a@k.gg>
2020-12-22 08:50:17 +0000

    Rework

--- asrtweet.c
+++ asrtweet.c
@@ -18,6 +18,92 @@
 #include <ajlparse.h>
 #include <ajl.h>
 
+void nameuser(FILE * o, j_t u)
+{                               // Write name of user (try full name is not emoji or too long, else @name)
+   const char *p = j_get(u, "name"),
+       *q;
+   if (p)
+      for (q = p; *q && !(*q & 0x80); q++);
+   if (!q || *q || q > p + 20)
+      fprintf(o, "@%s", j_get(u, "screen_name"));
+   else
+      fprintf(o, "%s", p);
+}
+
+int expand(FILE * o, j_t t)
+{                               // Expand a tweet (main tweet, quoted tweet, retweeted tweet... (does not line wrap at this point)
+   j_t j = j_find(t, "extended_tweet");
+   if (j_isobject(j))
+      t = j;                    // Use extended
+   j_t entities = j_find(t, "entities");
+   j_t text = j_find(t, "full_text") ? : j_find(t, "text");
+   if (!j_isstring(text))
+      return 0;
+   const char *i = j_val(text);
+   int pos = 0;                 // unicode position for replacement
+   int count = 0;               // count actual text
+   int skip = atoi(j_val(j_index(j_find(t, "display_text_range"), 0)) ? : "");  // Initial skipped text
+   while (*i)
+   {
+      if (skip)
+      {                         // Skip some text
+         skip--;
+         pos++;
+         i++;
+         while ((*i & 0xC0) == 0x80)
+            i++;
+         continue;
+      }
+      int match(j_t j) {
+         j = j_find(j, "indices");
+         if (!j_isarray(j) || j_len(j) != 2)
+            return 0;
+         int a = atoi(j_val(j_index(j, 0)));
+         if (a != pos)
+            return 0;
+         int b = atoi(j_val(j_index(j, 1)));
+         if (b <= a)
+            return 0;
+         return b - a;          // Length to skip - indicates found
+      }
+      for (j = j_first(j_find(entities, "urls")); !skip && j; j = j_next(j))
+         if ((skip = match(j)))
+            fprintf(o, "%s", j_get(j, "display_url"));
+      if (skip)
+         continue;
+      for (j = j_first(j_find(entities, "media")); !skip && j; j = j_next(j))
+         if ((skip = match(j)))
+            fprintf(o, "[%s]", j_get(j, "type"));
+      if (skip)
+         continue;
+      for (j = j_first(j_find(entities, "user_mentions")); !skip && j; j = j_next(j))
+         if ((skip = match(j)))
+            nameuser(o, j);
+      if (skip)
+         continue;
+      int check(const char *from, const char *to) {     // Simple replace
+         int l = strlen(from);
+         if (strncmp(i, from, l))
+            return 0;
+         fprintf(o, "%s", to);
+         while (l--)
+            if ((*i++ & 0xC0) != 0x80)
+               pos++;
+         return 1;
+      }
+      if (check("&amp;", "&") || check("&lt;", "<") || check("&gt;", ">") || check("£", "GBP"))
+         continue;
+      // Next character
+      count++;
+      pos++;
+      fputc(*i++, o);
+      while ((*i & 0xC0) == 0x80)
+         fputc(*i++, o);
+   }
+   fprintf(o, "\n");
+   return count;
+}
+
 int main(int argc, const char *argv[])
 {
    int debug = 0;
@@ -129,8 +215,6 @@ int main(int argc, const char *argv[])
          break;
       if (er)
          errx(1, "failed: %s", er);
-      if (debug)
-         j_err(j_write_pretty(j, stderr));
       long long id = strtoull(j_get(j, "user.id"), NULL, 0);
       int i = 0;
       if (idn)
@@ -139,180 +223,71 @@ int main(int argc, const char *argv[])
          if (i == idn)
             continue;
       }
-      unsigned long long tsms = strtoull(j_get(j, "timestamp_ms") ? : "", NULL, 0);
-      time_t ts = tsms / 1000;
-      struct tm tm;
-      gmtime_r(&ts, &tm);
-      char when[100] = "";
-      if (ts)
-      {
-         strftime(when, sizeof(when) - 5, "%FT%T", &tm);
-         sprintf(when + strlen(when), ".%03dZ", tsms % 1000);
-      } else
-         strncpy(when, j_get(j, "created_at") ? : "", sizeof(when));
-      const char *name = (i ? NULL : alias),
-          *at = "";
-      if (!name)
-      {
-         name = j_get(j, "user.name");
-         if (!name || !*name || strlen(name) > 20 || (*name & 0x80))
-         {
-            at = "@";
-            name = j_get(j, "user.screen_name");
-         }
-      }
-      j_t t = j_find(j, "retweeted_status") ? : j;
-      j_t entities = j_find(t, "extended_tweet.entities") ? : j_find(t, "entities");
-      char *in = strdup(j_get(t, "extended_tweet.full_text") ? : j_get(t, "full_text") ? : j_get(t, "text") ? : "");
-      int lenin = strlen(in),
-          lenout = lenin + 1;
-      char *out = malloc(lenout);
-      int text = 0;
-      int pos = 0;
-      int skip = atoi(j_val(j_index(j_find(t, "extended_tweet.display_text_range") ? : j_find(t, "display_text_range"), 0)) ? : "");;
-      {                         // WTF is twitter XML coding stuff in JSON, really. De-XML stuff
-         int i = 0,
-             o = 0;
-         while (in[i])
-         {
-            if (!skip)
-            {
-               inline void replace(int len, const char *new) {
-                  if (len < 0 || !new)
-                     return;
-                  int newlen = strlen(new);
-                  if (newlen != len)
-                     out = realloc(out, lenout += newlen - len);
-                  memcpy(out + o, new, newlen);
-                  o += newlen;
-                  for (int q = 0; q < len; q++)
-                     if ((in[i + q] & 0xC0) != 0x80)
-                        skip++;
-               }
-               inline int check(const char *from, const char *to) {
-                  int len = strlen(from);
-                  if (strncmp(in + i, from, len))
-                     return 0;  // Not found
-                  replace(len, to);     // Replace
-                  return 1;
-               }
-               if (entities)
-               {                // User mentions
-                  j_t u = NULL;
-                  for (u = j_first(j_find(entities, "user_mentions")); u; u = j_next(u))
-                  {
-                     j_t ind = j_find(u, "indices");
-                     if (j_isarray(ind) && j_len(ind) == 2 && atoi(j_val(j_index(ind, 0))) == pos)
-                     {
-                        const char *name = j_get(u, "name");
-                        if (!(*name & 0x80))
-                           replace(atoi(j_val(j_index(ind, 1))) - pos, j_get(u, "name"));
-                        else
-                           u = NULL;
-                        break;
-                     }
-                  }
-                  if (u)
-                     continue;  // found
-               }
-               if (entities)
-               {                // URLS
-                  j_t u = NULL;
-                  for (u = j_first(j_find(entities, "urls")); u; u = j_next(u))
-                  {
-                     j_t ind = j_find(u, "indices");
-                     if (j_isarray(ind) && j_len(ind) == 2 && atoi(j_val(j_index(ind, 0))) == pos)
-                     {
-                        replace(atoi(j_val(j_index(ind, 1))) - pos, j_get(u, "display_url"));
-                        break;
-                     }
-                  }
-                  if (u)
-                     continue;  // found
-               }
-               if (entities)
-               {                // media
-                  j_t m = NULL;
-                  for (m = j_first(j_find(entities, "media")); m; m = j_next(m))
-                  {
-                     j_t ind = j_find(m, "indices");
-                     if (j_isarray(ind) && j_len(ind) == 2 && atoi(j_val(j_index(ind, 0))) == pos)
-                     {
-                        char *t;
-                        if (asprintf(&t, "[%s]", j_get(m, "type")) < 0)
-                           errx(1, "malloc");
-                        replace(atoi(j_val(j_index(ind, 1))) - pos, t);
-                        free(t);
-                        break;
-                     }
-                  }
-                  if (m)
-                     continue;  // found
-               }
-               check("&amp;", "&");     // XML crap
-               check("&lt;", "<");
-               check("&gt;", ">");
-               check("£", "GBP");      // Cannot print £
-            }
-            pos++;              // Count unicode pos
-            if (!skip)
-            {
-               text++;          // Count normal text
-               out[o++] = in[i];
-            }
-            i++;
-            while ((in[i] & 0xC0) == 0x80)
-            {
-               if (!skip)
-                  out[o++] = in[i];
-               i++;
-            }
-            if (skip)
-               skip--;          // Skipping input
-         }
-         out[o] = 0;
-      }
-      free(in);
-      const char *place = j_get(j, "place.name");
-      if (!text)
-      {                         // No print printing if just media
-         free(out);
-         continue;
-      }
-      int more = 0;
-      if (strtoull(j_get(j, "in_reply_to_user_id"), NULL, 0) == strtoull(j_get(j, "user.id"), NULL, 0))
-         more = 1;
-      // Simple write out
+
+      int count = 0;
       char *msg = NULL;
-      size_t l = 0;
-      FILE *o = open_memstream(&msg, &l);
-      fprintf(o, "+++ %s %sFROM %s%s", when, more ? "MORE " : "", at, name);
-      if (place)
-         fprintf(o, " IN %s", place);
-      fprintf(o, " +++\n");
-      if (j != t)
-      {                         // Is a retweet
-         const char *name = j_get(t, "user.name"),
-             *at = "";
-         if (*name & 0x80)
+      size_t msglen = 0;
+      FILE *o = open_memstream(&msg, &msglen);
+      int self = (strtoull(j_get(j, "in_reply_to_user_id"), NULL, 0) == strtoull(j_get(j, "user.id"), NULL, 0));
+      {                         // Heading line
+         unsigned long long tsms = strtoull(j_get(j, "timestamp_ms") ? : "", NULL, 0);
+         time_t ts = tsms / 1000;
+         struct tm tm;
+         gmtime_r(&ts, &tm);
+         char when[100] = "";
+         if (ts)
          {
-            name = j_get(t, "user.screen_name");
-            at = "@";
-         }
-         fprintf(o, "[Retweet %s%s %s]\n", at, name, j_get(t, "created_at"));
+            strftime(when, sizeof(when) - 5, "%FT%T", &tm);
+            sprintf(when + strlen(when), ".%03dZ", tsms % 1000);
+         } else
+            strncpy(when, j_get(j, "created_at") ? : "", sizeof(when));
+         fprintf(o, "+++ %s", when);
+         if (self)
+            fprintf(o, " MORE");
+         fprintf(o, " FROM ");
+         nameuser(o, j_find(j, "user"));
+         const char *place = j_get(j, "place.name");
+         if (place)
+            fprintf(o, " IN %s", place);
+         fprintf(o, " +++\n");
       }
-      if (!more)
-      {                         // Reply
+      if (!self)
+      {                         // reply to
          j_t reply = j_find(j, "in_reply_to_screen_name");
          if (j_isstring(reply))
-            fprintf(o, "[In reply to @%s]\n", j_val(reply));
+            fprintf(o, "[IN REPLY TO @%s]\n", j_val(reply));
       }
-      for (unsigned char *p = out; *p; p++)
+      j_t retweet = j_find(j, "retweet_status");
+      if (j_isobject(retweet))
+      {
+         fprintf(o, "[RETWEET ");
+         nameuser(o, j_find(retweet, "user"));
+         fprintf(o, " %s]\n", j_get(retweet, "created_at"));
+         count += expand(o, retweet);
+      } else
+      {
+         count += expand(o, j);
+         if (!i && footnote)
+            fprintf(o, "*** %s ***\n", footnote);
+      }
+      j_t quoted = j_find(j, "quoted_status");
+      if (j_isobject(quoted))
+      {
+         fprintf(o, "[QUOTING ");
+         nameuser(o, j_find(quoted, "user"));
+         fprintf(o, " %s]\n", j_get(quoted, "created_at"));
+         count += expand(o, quoted);
+      }
+      fprintf(o, "\n\n");       // Gap
+      fclose(o);
+      for (unsigned char *p = msg; *p; p++)
          if (p[0] == 0xE2 && p[1] == 0x80 && p[2] == 0xA6)
             p[0] = p[1] = p[2] = '.';   // Ellipsis, done here as used in expanded URLs
       if (wrap)
-      {                         // Line wrap
-         char *p = out;
+      {                         // Line wrapping
+         char *was = msg,
+             *p = msg;
+         FILE *o = open_memstream(&msg, &msglen);
          while (*p)
          {
             int pos = 0;
@@ -334,33 +309,18 @@ int main(int argc, const char *argv[])
             if (*p == '\n')
                p++;
          }
-      } else
-         fprintf(o, "%s\n", out);
-      if (!i && footnote && j == t)
-         fprintf(o, "*** %s ***\n", footnote);
-      {                         // Quoted
-         j_t quoted = j_find(j, "quoted_status");
-         if (quoted)
-         {
-            const char *name = j_get(quoted, "user.name"),
-                *at = "";
-            if (*name & 0x80)
-            {
-               name = j_get(quoted, "user.screen_name");
-               at = "@";
-            }
-            fprintf(o, "[Quoted %s%s %s]\n", at, name, j_get(quoted, "created_at"));
-         }
+         fclose(o);
+         free(was);
       }
-      fprintf(o, "\n\n");
-      fclose(o);
-      int e = mosquitto_publish(mqtt, NULL, topic, l, msg, 0, 0);
-      if (e)
-         warnx("MQTT publish failed %s (%s) %d bytes", mosquitto_strerror(e), topic, l);
-      if (debug)
-         fprintf(stderr, "%s", msg);
+      if (count)
+      {
+         int e = mosquitto_publish(mqtt, NULL, topic, msglen, msg, 0, 0);
+         if (e)
+            warnx("MQTT publish failed %s (%s) %d bytes", mosquitto_strerror(e), topic, msglen);
+         if (debug)
+            fprintf(stderr, "%s", msg);
+      }
       free(msg);
-      free(out);
    }
    j_free(j);
    return 0;
