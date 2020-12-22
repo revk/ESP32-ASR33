f01ce6fac426ad6ca4f544d2b2ce86f8d4d4df0f
Adrian Kennard <a@k.gg>
2020-12-22 08:55:28 +0000

    More mappings

--- asrtweet.c
+++ asrtweet.c
@@ -91,7 +91,7 @@ int expand(FILE * o, j_t t)
                pos++;
          return 1;
       }
-      if (check("&amp;", "&") || check("&lt;", "<") || check("&gt;", ">") || check("£", "GBP"))
+      if (check("&amp;", "&") || check("&lt;", "<") || check("&gt;", ">") || check("£", "GBP") || check("“", "\"") || check("”", "\"") || check("—", "-") || check("{", "[") || check("}", "]"))
          continue;
       // Next character
       count++;
