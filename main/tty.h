a80da4217f1790f20e6ed82658c9973cf69bf932
RevK <github@e.gg>
2023-08-14 07:10:27 +0100

    Library update

--- main/tty.h
+++ main/tty.h
@@ -1,7 +1,7 @@
 // UART low level
 
-#define port_mask(p) ((p)&0x3F) // Get GPIO from io()
-#define port_inv(p) (((p)&0x40)?1:0)    // Get if inverted
+#define port_mask(p) ((p)&0x3FFF) // Get GPIO from io()
+#define port_inv(p) (((p)&0x4000)?1:0)    // Get if inverted
 
 void tty_setup(void);
 void tty_flush(void);
